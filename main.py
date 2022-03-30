8b7e8cfebf9522e756265a4c1050d2cada124a64
Hu Bowen <85286288+wHo69@users.noreply.github.com>
2022-03-30 13:09:44 +0000

    Added image uploading support

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, jsonify, make_response
+from werkzeug.utils import secure_filename
+
 import json
+import os
 
 app = Flask(__name__)
 
@@ -30,30 +33,60 @@ def get_data():
 
 @app.route('/api/update_data', methods=['POST'])
 def update_data():
-    attire = request.form['attire']
-    venue = request.form['venue']
-    timing = request.form['timing']
+    if request.method == "POST":
+        # Get values
+        attire = request.form['attire']
+        venue = request.form['venue']
+        timing = request.form['timing']
+        date = request.form['date'] # Date in form of yyyy-mm-dd
 
-    # Input formatting
-    if attire == "":
-        attire = "Empty Value Received"
-    if venue == "":
-        venue = "Empty Value Received"
-    if timing == "":
-        timing = "Empty Value Received"
+        timetable = request.files['timetable']
+        fname = secure_filename(timetable.filename)
 
-    data = {
-        "attire": attire,
-        "venue": venue,
-        "time": timing
-    }
+        # Parse Date
+        dateParsed = date.split("-")
+        dateParsed = dateParsed[2] + "/" + dateParsed[1] + "/" + dateParsed[0][2:]
 
-    with open("training_data.json", "w") as f:
-        json.dump(data, f, indent=4)
-        f.close()
+        # Dump values into dict
+        data = {
+            "attire": attire,
+            "venue": venue,
+            "time": timing,
+            "date": dateParsed,
+            "imgSrc": "https://ncctrainingupdater.hubowen.repl.co/static/images/" + str(fname)
+        }
 
-    return render_template("updated.html")
+        # Update values
+        with open("training_data.json", "w") as f:
+            json.dump(data, f, indent=4)
+            f.close()
 
+        # Get persistent data
+        # prevImage: used to delete former image
+        with open("persistent_data.json", "r") as f:
+            persistent_data = json.load(f)
+            f.close()
+
+        # Get prevImg and delete
+        prevImg = persistent_data["prevImage"]
+        try:
+            os.remove(os.path.join(app.root_path, 'static', 'images', prevImg))
+        except:
+            print("[ERROR] Couldn't delete file!")
+            
+        # Save image
+        try:
+            timetable.save(os.path.join(app.root_path, 'static', 'images',  fname))
+        except Exception as e:
+            return render_template("errorUpload.html", errorLog=e)
+
+        # Update prevImg to current and save
+        persistent_data["prevImage"] = fname
+        with open("persistent_data.json", "w") as f:
+            json.dump(persistent_data, f, indent=4)
+
+        # return successfully updated
+        return render_template("updated.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
